bbf83d75c8bfc7866acdd9f6787f23a627066c2f
4rauj6 <joaovas123m@gmail.com>
2026-09-01 10:54:11 -0300

    New files has been added in Assets' folder. Website's body and elements color has been changed. On hover function added to the first section

--- src/components/functions/aacvmDisplay.jsx
+++ src/components/functions/aacvmDisplay.jsx
@@ -14,7 +14,7 @@ function AcvmDisplay() {
           <img
             src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.independent.co.uk%2F2023%2F11%2F03%2F12%2FnewFile.jpg&f=1&nofb=1&ipt=c9718efc525bf0fb415b42082d153e9209f11187072c99dd73d142704d6cb4d5"
             alt="FA Cup Trophy"
-            className="w-115 shrink-0 rounded "
+            className="w-114 shrink-0 rounded "
           />
           <div
             className={`flex flex-col bg-blue-500 p-4 transition duration-300 ease-in-out rounded w-full h-25 absolute bottom-0 ${
@@ -23,7 +23,7 @@ function AcvmDisplay() {
                 : "translate-y-10 opacity-0"
             }`}
           >
-            <div className="flex flex-col justify-center items-center h-12">
+            <div className="flex flex-col justify-center items-center">
               <span className="text-xl">The FA Cup</span>
               <span>
                 2018/2019 - <a href="https://chelseafc.com">Chelsea FC</a>
@@ -37,9 +37,9 @@ function AcvmDisplay() {
           onMouseLeave={() => setIsOnHover(null)}
         >
           <img
-            src="src/assets/efl-championship-trophy-2341761766.jpg"
-            alt="FA Cup Trophy"
-            className="w-115 shrink-0 rounded"
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.getfootballnewsgermany.com%2Fassets%2Fbayer-04-leverkusen-v-fc-st-pauli-dfb-cup-quarter-final-1-scaled.jpg&f=1&nofb=1&ipt=fff142d7c17658f327294cd439b6918c3f166d5679a38114fb701ae6e77e4c23"
+            alt="DFB Cup Trophy"
+            className="w-114 shrink-0 rounded"
           />
           <div
             className={`flex flex-col bg-blue-500 p-8 transition duration-300 ease-in-out rounded  w-full h-25 absolute bottom-0 ${
@@ -47,7 +47,15 @@ function AcvmDisplay() {
                 ? "translate-y-0  opacity-100"
                 : "translate-y-10 opacity-0"
             }`}
-          />
+          >
+            <div className="flex flex-col justify-center items-center h-5">
+              <span className="text-xl">The DFB Pokal</span>
+              <span>
+                2015/2016 -{" "}
+                <a href="https://www.bayer04.de">Bayern Munich FC</a>
+              </span>
+            </div>
+          </div>
         </div>
 
         <div
@@ -56,9 +64,9 @@ function AcvmDisplay() {
           onMouseLeave={() => setIsOnHover(null)}
         >
           <img
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.independent.co.uk%2F2023%2F11%2F03%2F12%2FnewFile.jpg&f=1&nofb=1&ipt=c9718efc525bf0fb415b42082d153e9209f11187072c99dd73d142704d6cb4d5"
+            src="src/assets/efl-championship-trophy-2341761766.jpg"
             alt="FA Cup Trophy"
-            className="w-115 shrink-0 rounded"
+            className="w-113 shrink-0 rounded"
           />
           <div
             className={`flex flex-col bg-blue-500 p-8 transition duration-300 ease-in-out rounded  w-full h-25 absolute bottom-0 ${
@@ -66,7 +74,14 @@ function AcvmDisplay() {
                 ? "translate-y-0  opacity-100"
                 : "translate-y-10 opacity-0"
             }`}
-          />
+          >
+            <div className="flex flex-col justify-center items-center h-5">
+              <span className="text-xl">EFL Championship</span>
+              <span>
+                2013/2014 - <a href="https://www.lcfc.com">Leicester City FC</a>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import { FaFacebookSquare } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Achievements from "./Achievements";
 import AcvmDisplay from "../functions/aacvmDisplay";
+import { useState } from "react";
 
 function Homepage() {
   const slides = [
@@ -11,8 +12,10 @@ function Homepage() {
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclamorworld.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fshoesfootball-1350775_1920.jpg&f=1&nofb=1&ipt=63d6d7c1fd884987228bc6a420a58c79c9f7a4972a7a1928087e153b19b3c48b",
   ];
 
+  const [isOnHover, setIsOnHover] = useState(null);
+
   return (
-    <div className="">
+    <div className="bg-gray-50">
       <section className="text-blue-500 ">
         <div>
           <h1>Athlete&apos;s name</h1>
@@ -21,7 +24,9 @@ function Homepage() {
             failure, every obstacle it was necessary to create what he&apos;s
             are today
           </p>
-          <button>Know more</button>
+          <button className="bg-blue-500 text-white p-1 rounded cursor-pointer">
+            Know more
+          </button>
         </div>
 
         <div>
@@ -29,42 +34,100 @@ function Homepage() {
         </div>
       </section>
 
-      <section className="text-blue-500">
-        <h1>Knowing more...</h1>
-
-        <div>
+      <section className="">
+        <div className="p-15">
+          <span className="text-blue-500">Discover more about me here</span>
+          <h1 className="text-blue-500 text-5xl font-kaput">Welcome!</h1>
+        </div>
+        <div className="flex flex-row gap-10 object-cover justify-center items-center">
           <div>
             <Link to="/biografy">
-              <img src="src/assets/react.svg" alt="History page img" />
-              <span>
-                <strong>Biografy</strong>
-              </span>
-              <p>Knowing deeply inside of Athlete&apos;s biografy</p>
+              <div
+                className="relative"
+                onMouseEnter={() => setIsOnHover(1)}
+                onMouseLeave={() => setIsOnHover(null)}
+              >
+                <img
+                  src="src/assets/back-view-of-a-latin-american-soccer-player-holding-a-soccer-ball-while-walking-at-the-soccer-573744466.jpg"
+                  alt="History page img"
+                  className="w-105 rounded border-blue-500 border-2"
+                />
+
+                <div
+                  className={`flex flex-col bg-blue-500 p-4 transition duration-300 ease-in-out w-full h-30 absolute bottom-0 ${isOnHover === 1 ? "translate-y-0 opacity-100" : "translate-y-4 opacity-0"}`}
+                >
+                  <div className="text-white">
+                    <span className="text-3xl">
+                      <strong>Biografy</strong>
+                    </span>
+                    <p>Knowing deeply inside of my life</p>
+                    <p>Click to know more about it</p>
+                  </div>
+                </div>
+              </div>
             </Link>
           </div>
 
           <div>
-            <img src="" alt="History page img" />
-            <span>
-              <strong>Titles</strong>
-            </span>
-            <p></p>
+            <div
+              className="relative"
+              onMouseEnter={() => setIsOnHover(2)}
+              onMouseLeave={() => setIsOnHover(null)}
+            >
+              <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fe6.365dm.de%2F18%2F08%2F1600x900%2Fskysport_de-dfb-pokal-trophe_4391592.jpg%3F20180816140458&f=1&nofb=1&ipt=40e1ae8186bc2dd1be6299be34a1956652adf0608b674f4dc1e9b6dc8e0d035f"
+                alt="History page img"
+                className="w-115 rounded border-blue-500 border-2"
+              />
+              <div
+                className={`flex flex-col bg-blue-500 p-4 transition duration-300 ease-in-out w-full h-30 absolute bottom-0 ${isOnHover === 2 ? "translate-y-0 opacity-100" : "translate-y-4 opacity-0"}`}
+              >
+                <div className="text-white ">
+                  <span className="text-3xl">
+                    <strong>Titles</strong>
+                  </span>
+                  <p>Knowing deeply inside of Athlete&apos;s titles</p>
+                  <p>Click to know more about it</p>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div>
-            <img src="" alt="History page img" />
-            <span>
-              <strong>Biografy</strong>
-            </span>
-            <p>Knowing deeply inside of Athlete&apos;s biografy</p>
+            <div
+              className="relative"
+              onMouseEnter={() => setIsOnHover(3)}
+              onMouseLeave={() => setIsOnHover(null)}
+            >
+              <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.awbclaw.co.uk%2Fwp-content%2Fuploads%2F2023%2F04%2FCharity.jpg&f=1&nofb=1&ipt=751bdaa0bee051019114dfde1e5c6dc8fab500fcdb2c8ad6efe6b41d5895db04"
+                alt="History page img"
+                className="w-105 rounded border-blue-500 border-2"
+              />
+              <div
+                className={`pointer-events-none flex flex-col bg-blue-500 p-4 transition duration-300 ease-in-out w-full h-30 absolute bottom-0 ${isOnHover === 3 ? "translate-y-0 opacity-100" : "translate-y-4 opacity-0"}`}
+              >
+                <div className="text-white">
+                  <span className="text-3xl">
+                    <strong>Charity</strong>
+                  </span>
+                  <p>
+                    Knowing deeply inside of Athlete&apos;s charities collabs
+                  </p>
+                  <p>
+                    <p>Click to know more about it</p>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
 
-      <section>
+      <section className="bg-blue-500  h-250 mt-30">
         <div className="flex self-start p-15 flex-col">
-          <span>Medias - the best moments</span>
-          <h1 className="text-5xl font-kaput">Gallery</h1>
+          <span className="text-white">Medias - the best moments</span>
+          <h1 className="text-5xl font-kaput text-white">Gallery</h1>
         </div>
 
         <div className="flex justify-center">
@@ -72,7 +135,7 @@ function Homepage() {
         </div>
       </section>
 
-      <section>
+      <section className="bg-gray-50">
         <div>
           <div className="flex self-start p-15 flex-col">
             <span>Top achievements</span>
@@ -91,7 +154,7 @@ function Homepage() {
         </div>
       </section>
 
-      <section>
+      <section className="bg-blue-500">
         <div>
           <div className="flex self-start p-15 flex-col">
             <span>Social medias - stay connected </span>
